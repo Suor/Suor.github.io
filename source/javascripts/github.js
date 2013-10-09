a5ca6298011de3001adbc0b7781f1a51f1ec703a
Alexander Schepanovski <suor.web@gmail.com>
2013-10-09 23:49:17 +0800

    Don't show certain github repos in aside block

--- source/javascripts/github.js
+++ source/javascripts/github.js
@@ -21,6 +21,7 @@ var github = (function(){
           if (!data || !data.data) { return; }
           for (var i = 0; i < data.data.length; i++) {
             if (options.skip_forks && data.data[i].fork) { continue; }
+            if (options.skip && options.skip.indexOf(data.data[i].name) != -1) { continue; }
             repos.push(data.data[i]);
           }
           if (options.count) { repos.splice(options.count); }
